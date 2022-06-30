766d8e91d40ac8ac31c3e2d9e97bb16a9e4bef37
elogrus <elogrus@gmail.com>
2022-06-30 22:10:05 +0500

    v3
    Make up with adaptive.

--- assets/script.js
+++ assets/script.js
@@ -51,21 +51,69 @@ function headerRunnerLeave(event) {
 
 startRunnerElement();
 
-document.querySelectorAll(".header__list-item").forEach((elem) => {
-    elem.addEventListener("mouseenter", headerRunnerEnter);
-    elem.addEventListener("mouseleave", headerRunnerLeave);
-});
+// burger-menu appends from width <= 450
+if (window.screen.width > 450) {
+    document.querySelectorAll(".header__list-item").forEach((elem) => {
+        elem.addEventListener("mouseenter", headerRunnerEnter);
+        elem.addEventListener("mouseleave", headerRunnerLeave);
+    });
+}
 
 // * ------- * //
 
 function scrollTo(event) {
-    console.log(!!event);
-    let pos =
-        event.target.getBoundingClientRect().top +
-        document.documentElement.scrollTop +
-        event.target.offsetHeight;
+    let pos = event.currentTarget.parentNode.offsetHeight;
     window.scroll(0, pos);
 }
 document.querySelectorAll("#scroller").forEach((elem) => {
     elem.addEventListener("mousedown", scrollTo);
 });
+
+// * --------------------------- * //
+
+function burger() {
+    // burger-menu appends from width <= 450
+    if (window.screen.width > 450) return;
+
+    let burger = document.getElementById("burger");
+    let burgerModal = document.getElementById("burger-menu");
+    function onBurgerClick(event) {
+        if (burger.dataset.opened == "true") {
+            burger.dataset.opened = false;
+            burgerModal.dataset.opened = false;
+        } else {
+            burger.dataset.opened = true;
+            burgerModal.dataset.opened = true;
+        }
+    }
+    burger.addEventListener("mousedown", onBurgerClick);
+}
+
+burger();
+
+// * --------------------------- * //
+function hideHeaderOnScroll() {
+    let currentScroll = 0;
+    let header = document.getElementById("header");
+    let burger = document.getElementById("burger");
+    let burgerTop = burger.offsetTop;
+    let burgerModal = document.getElementById("burger-menu");
+    function onScroll(event) {
+        console.log("scroll");
+        let nowScroll = document.documentElement.scrollTop;
+        if (nowScroll > currentScroll && burgerModal.dataset.opened != "true") {
+            header.style.top = "-100%";
+            burger.style.top = "-100%";
+        } else if (
+            nowScroll < currentScroll &&
+            burgerModal.dataset.opened != "true"
+        ) {
+            header.style.top = "0";
+            burger.style.top = burgerTop + "px";
+        }
+        currentScroll = nowScroll;
+    }
+    // when site was loaded, user start scroll and he may not see that header exists, so I set 2s delay
+    setTimeout(() => window.addEventListener("scroll", onScroll), 2000);
+}
+hideHeaderOnScroll();
